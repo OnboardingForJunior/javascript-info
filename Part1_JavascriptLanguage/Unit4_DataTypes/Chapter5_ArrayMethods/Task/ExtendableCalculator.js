dac5310a997f54f3b4b658adc0c2d3e191823054
sunbang123 <musicianksy@naver.com>
2022-08-21 00:45:49 +0900

    계산기 알고리즘 문제 풀이 진행중입니다.

--- Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/ExtendableCalculator.js
+++ Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/ExtendableCalculator.js
@@ -4,24 +4,41 @@
 
 // Calculator는 두 단계를 거쳐 만들 수 있습니다.
 
-// 첫 번째 단계는 "1 + 2"와 같은 문자열을 받아서 “숫자 연산자 숫자” 형태(공백으로 구분)로 바꿔주는 메서드 calculate(str)를 구현하는 것입니다. 이 함수는 +와 -를 처리할 수 있어야 하고, 연산 결과를 반환해야 합니다.
+// 첫 번째 단계는 "1 + 2"와 같은 문자열을 받아서 “숫자 연산자 숫자” 형태(공백으로 구분)로 바꿔주는
+// 메서드 calculate(str)를 구현하는 것입니다. 이 함수는 +와 -를 처리할 수 있어야 하고, 연산 결과를 반환해야 합니다.
 
 // 예시:
 
-let calc = new Calculator;
+const calc = new Calculator;
 
-alert( calc.calculate("3 + 7") ); // 10
-// 두 번째 단계는 계산기가 새로운 연산을 학습할 수 있도록 해주는 메서드 addMethod(name, func)를 추가해 주는 것입니다. 연산자 이름을 나타내는 name과 인수가 두개인 익명 함수 func(a,b)를 받는 새 메서드를 구현해야 하죠.
+function Calculator(str){
+    const newArr = `${str}`.split(" ");
+    const sumNumbers = (sum, current) => sum + current;
+
+    newArr.forEach((item, index, array) => {
+        if(!isNaN(+item)) {
+            item = index == 1 ? +item : +(array[index-1] + item);
+        }
+    });
+
+    return newArr.reduce(sumNumbers);
+}
+
+calc.calculate = Calculator;
+
+console.log( calc.calculate("3 + 7 - 6") ); // 10
+// 두 번째 단계는 계산기가 새로운 연산을 학습할 수 있도록 해주는 메서드 addMethod(name, func)를 추가해 주는 것입니다.
+// 연산자 이름을 나타내는 name과 인수가 두개인 익명 함수 func(a,b)를 받는 새 메서드를 구현해야 하죠.
 
 // 구현된 메서드를 이용해 곱셈 *과 나눗셈 /, 거듭제곱 **연산자를 추가해주는 예시는 아래와 같습니다.
 
-let powerCalc = new Calculator;
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
+// const powerCalc = new Calculator;
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
 
-let result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
+// const result = powerCalc.calculate("2 ** 3");
+// console.log( result ); // 8
 // 참고사항:
 
 // 괄호나 복잡한 표현식 없이도 본 과제를 풀 수 있습니다.
