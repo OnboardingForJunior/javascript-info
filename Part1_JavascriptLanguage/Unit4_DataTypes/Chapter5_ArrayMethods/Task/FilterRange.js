1591daf279996c46416363c014e5b572552c49f5
sunbang123 <musicianksy@naver.com>
2022-08-20 18:30:58 +0900

    특정 범위를 필터링 하는 알고리즘 문제 해결후 업로드합니다.

--- Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/FilterRange.js
+++ Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/FilterRange.js
@@ -7,10 +7,16 @@
 
 // 예시:
 
-let arr = [5, 3, 8, 1];
+const arr = [5, 3, 8, 1];
 
-let filtered = filterRange(arr, 1, 4);
+function filterRange(arr, a, b){
+    const newArr = arr.filter(items => items >= a && items <= b);
 
-alert( filtered ); // 3,1 (조건에 맞는 요소)
+    return newArr;
+}
 
-alert( arr ); // 5,3,8,1 (기존 배열은 변경되지 않았습니다.)
+const filtered = filterRange(arr, 1, 4);
+
+console.log( filtered ); // 3,1 (조건에 맞는 요소)
+
+console.log( arr ); // 5,3,8,1 (기존 배열은 변경되지 않았습니다.)
