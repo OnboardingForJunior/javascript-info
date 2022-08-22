c47b71d6bb337f22b2824db74b8fb12255fa0409
sunbang123 <musicianksy@naver.com>
2022-08-22 20:15:17 +0900

    나이 기준으로 객체 정리하는 알고리즘 문제 해결 후 업로드합니다.

--- Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/SortUsersAge.js
+++ Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/SortUsersAge.js
@@ -4,15 +4,19 @@
 
 // 예시:
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+const john = { name: "John", age: 25 };
+const pete = { name: "Pete", age: 30 };
+const mary = { name: "Mary", age: 28 };
 
-let arr = [ pete, john, mary ];
+const arr = [ pete, john, mary ];
+
+function sortByAge(arr){
+    arr.sort((a, b) => a.age - b.age);
+}
 
 sortByAge(arr);
 
 // now: [john, mary, pete]
-alert(arr[0].name); // John
-alert(arr[1].name); // Mary
-alert(arr[2].name); // Pete
+console.log(arr[0].name); // John
+console.log(arr[1].name); // Mary
+console.log(arr[2].name); // Pete
