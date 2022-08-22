0eafcad5a794ab3364d014801e967815cfc73b1a
sunbang123 <musicianksy@naver.com>
2022-08-22 17:16:44 +0900

    매핑 알고리즘 문제 2개 해결 후 업로드합니다.

--- Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/MapToObjects.js
+++ Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/MapToObjects.js
@@ -2,7 +2,8 @@
 // 중요도: 5
 // 세 개의 프로퍼티 name과 surname, id를 가진 객체 user가 담긴 배열이 있습니다.
 
-// name과 surname을 조합해 fullName을 만들고, 이를 이용해 두 개의 프로퍼티 id와 fullName을 가진 객체를 담은 새로운 배열을 반환해주는 코드를 작성해보세요.
+// name과 surname을 조합해 fullName을 만들고,
+// 이를 이용해 두 개의 프로퍼티 id와 fullName을 가진 객체를 담은 새로운 배열을 반환해주는 코드를 작성해보세요.
 
 // 예시:
 
@@ -12,7 +13,14 @@ let mary = { name: "Mary", surname: "Key", id: 3 };
 
 let users = [ john, pete, mary ];
 
-let usersMapped = /* 여기에 코드를 작성하세요. */
+let usersMapped = users.map((item, index, array) => {
+  item = {
+    fullName: array[index].name + " " + array[index].surname,
+    id : array[index].id,
+  }
+
+  return item;
+});
 
 /*
 usersMapped = [
@@ -22,6 +30,6 @@ usersMapped = [
 ]
 */
 
-alert( usersMapped[0].id ) // 1
-alert( usersMapped[0].fullName ) // John Smith
+console.log( usersMapped[0].id ) // 1
+console.log( usersMapped[0].fullName ) // John Smith
 // 문제를 해결하려면 배열을 새로운 배열로 매핑해야 합니다. 힌트를 하나 드리자면 =>를 이용하는 것입니다.
