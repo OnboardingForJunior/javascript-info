180431662fcc21305f5a395a2ec09a9c27e720a9
sunbang123 <musicianksy@naver.com>
2022-08-23 14:04:39 +0900

    배열 무작위로 섞는 알고리즘 문제 해결합니다.

--- Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/ShuffleArray.js
+++ Part1_JavascriptLanguage/Unit4_DataTypes/Chapter5_ArrayMethods/Task/ShuffleArray.js
@@ -4,17 +4,27 @@
 
 // shuffle을 여러 번 실행하면 요소의 정렬 순서가 달라야 합니다. 예시를 살펴봅시다.
 
-let arr = [1, 2, 3];
+const arr = [1, 2, 3];
 
 shuffle(arr);
 // arr = [3, 2, 1]
+console.log(arr);
 
 shuffle(arr);
 // arr = [2, 1, 3]
+console.log(arr);
 
 shuffle(arr);
 // arr = [3, 1, 2]
+console.log(arr);
 // ...
 // 문제를 풀 때 주의할 점은 모든 순열이 동일한 확률로 일어나야 한다는 점입니다.
 // 예를 들어 [1,2,3] 은 [1,2,3]이나 [1,3,2], [3,1,2]로 재정렬 될 수 있는데
 // 이 배열들이 만들어지는 빈도는 같아야 합니다.
+
+function shuffle(arr){ 
+    // Math.random() => 0 ~ 1
+    // "".localeCompare() // => -1, 0, 1
+
+    arr.sort((a, b) => Math.random() - 0.5);
+}
