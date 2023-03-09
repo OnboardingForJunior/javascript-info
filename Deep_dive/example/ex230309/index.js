170dd5396e95317b820640e63b516eb0ba749e94
sunbang123 <musicianksy@naver.com>
2023-03-09 22:41:50 +0900

    모던자바스크립트 Deep Dive 예제 업로드합니다.

---
+++ Deep_dive/example/ex230309/index.js
@@ -0,0 +1,31 @@
+// const arr = [1, 2, 3];
+// // const arr = [2, 2, 3];
+
+
+// // 반복적으로 세번 전달하도록 하세요~
+// function print(v, idx, arr) {
+// 	console.log("arr: ", v, idx, arr); // v는 value
+// }
+// // arr.forEach(print); // 모든 배열 원소 하나하나를 함수에 전달해주세요.
+// arr.forEach(console.log);
+// arr.forEach((v)=>console.log("arr: ", v)); // 람다 문법입니다.
+// // 함수를 인수로 넘겨서... 첫인수 둘째인수 셋째인수! JS의 특징 - 유연함
+
+// for(let i=0;i<arr.length;++i)
+//     console.log("arr: ", arr[i]); // inline코드 함수형 코드.
+
+// const arr = [1, 2, 3];
+
+// arr.forEach(alert);
+
+// const numbers = [1, 2, 3];
+
+// numbers.forEach((number) => {
+//   console.log(number);
+// });
+
+const numbers = [1, 2, 3];
+
+numbers.forEach((number) => {
+  alert(number);
+});
